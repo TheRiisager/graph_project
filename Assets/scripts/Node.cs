a91d357792a7686c763de58ce1b28f7a7d12e9e2
Frederik Johnsen <frederik@familienjohnsen.dk>
2023-04-19 18:35:07 +0200

    a

---
+++ Assets/scripts/Node.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    [SerializeField]
+    private List<Node> list = new List<Node>();
+    private Dictionary<float, Node> connections = new Dictionary<float, Node>();
+    private float fCost;
+    private Node parent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //precalculate distance to connected nodes on init
+        foreach (Node node in list)
+        {  
+            float distance = Vector3.Distance(node.GetTransform().position, this.transform.position);
+            connections.Add(distance, node);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Transform GetTransform()
+    {
+        return this.gameObject.transform;
+    }
+
+    public float getFCost()
+    {
+        return this.fCost;
+    }
+
+    public void setFCost(float cost)
+    {
+        this.fCost = cost;
+    }
+
+    public Node getParent()
+    {
+        return this.parent;
+    }
+
+    public void setParent(Node node)
+    {
+        this.parent = node;
+    }
+
+    public Dictionary<float, Node> getConnections()
+    {
+        return this.connections;
+    }
+}
---
+++ Assets/scripts/agent.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class agent : MonoBehaviour
+{
+    [SerializeField]
+    private Node startNode;
+    [SerializeField]
+    private Node targetNode;
+    private bool atTarget = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(move());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(atTarget)
+        {
+            StopAllCoroutines();
+        }
+    }
+
+    //coroutine to move towards target every 5 seconds using A* algorithm
+    IEnumerator move()
+    {
+        yield return new WaitForSeconds(5f);
+        List<Node> openList = new List<Node>();
+        List<Node> closedList = new List<Node>();
+        
+        openList.Add(startNode);
+        this.startNode.setFCost(Vector3.Distance
+            (
+                this.startNode.transform.position,
+                this.targetNode.transform.position
+            ));
+
+        while (true) {
+            openList = openList.OrderBy(node => node.getFCost()).ToList();
+            Node current = openList[0];
+            openList.RemoveAt(0);
+            closedList.Add(current);
+
+            if (current.Equals(this.targetNode))
+            {
+                this.atTarget = true;
+                break;
+            }
+
+            foreach (var item in current.getConnections())
+            {
+                
+            }
+        }
+    }
+}
